67eb392a9a72bbb986818cf752fcddd9bcee700e
Suraj kumar <ksurajkumar336@gmail.com>
2026-05-17 22:16:17 +0530

    first commit

--- apps.js
+++ apps.js
@@ -1,31 +1,38 @@
-let menu = document.querySelector("#menuToggle");
+let menuBtn = document.querySelector("#menuToggle");
 let navLinks = document.querySelector("#navLinks");
 
 
-menu.addEventListener("click",()=> {
+// menu.addEventListener("click",()=> {
+//   navLinks.classList.toggle("active");
+// });
+
+// // emailjs
+
+// (function(){
+//   emailjs.init("4uNB190iSrEY1qN_U"); // EmailJS se milega
+// })();
+
+// document.getElementById("conatctForm").addEventListener("submit", function(e) {
+//   e.preventDefault();
+
+//   emailjs.send("service_gtan7tc", "template_uy1k4ps", {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     message: document.getElementById("message").value
+//   })
+//   .then(() => {
+//     alert("Message sent successfully 🚀");
+//   })
+//   .catch(() => {
+//     alert("Failed to send message ❌");
+//   });
+
+//   this.reset();
+// });
+
+// let menuBtn = document.querySelector(".menu-btn");
+// let navLinks = document.querySelector("#navLinks");
+
+menuBtn.addEventListener("click",()=> {
   navLinks.classList.toggle("active");
-});
-
-// emailjs
-
-(function(){
-  emailjs.init("4uNB190iSrEY1qN_U"); // EmailJS se milega
-})();
-
-document.getElementById("conatctForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  emailjs.send("service_gtan7tc", "template_uy1k4ps", {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  })
-  .then(() => {
-    alert("Message sent successfully 🚀");
-  })
-  .catch(() => {
-    alert("Failed to send message ❌");
-  });
-
-  this.reset();
 });
